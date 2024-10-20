5deed27f40697666b43f8328abd9e0c064054d4c
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2024-10-20 18:13:20 +0330

    feat(home): integrate react-router-dom and import components for routing
    - import Navbar
    - import Header
    - import WorkStation

---
+++ my-portfolio/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+import WorkStation from '../components/WorkStation'
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <WorkStation />            
+
+
+        </>
+    )
+}
