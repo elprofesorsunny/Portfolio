f2e6f2f30f2e7d73551bb90fc59de57da50157c6
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2024-10-20 18:19:50 +0330

    feat: add routing with react-router-dom and update page structure
    - Added react-router-dom for handling page navigation.
    - Implemented routing for the Home, Works, About, Contact, and Blogs pages using <Routes> and <Route>.
    - Updated the App component to wrap content inside <Router> and replaced static components with route-based pages.

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,14 +1,25 @@
-import Header from "./components/Header"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar"
+import Works from './pages/Works';
+import About from './pages/About';
+import Contact from "./pages/Contact";
+import Blogs from "./pages/Blogs";
+import Home from './pages/Home';
 
-function App() {
 
+export default function App() {
   return (
-    <div className="overflow-x-hidden">
-      <Navbar />
-      <Header />
-    </div>
-  )
+    <Router>
+      <div className='overflow-x-hidden'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-
-export default App
